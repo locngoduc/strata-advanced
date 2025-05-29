c9b73348ea897a4856977bcb7dbd7028d34481ac
locngoduc <vaderngo@gmail.com>
2025-05-30 00:16:15 +0700

    check database driver

--- api/pages/info.php
+++ api/pages/info.php
@@ -1,3 +1,13 @@
 <?php
-phpinfo();
+if (extension_loaded('pdo_mysql')) {
+    echo "PDO MySQL driver is installed and loaded.";
+} else {
+    echo "PDO MySQL driver is not installed or not loaded.";
+}
+
+if (extension_loaded('pdo_pgsql')) {
+    echo "PDO PostgreSQL driver is installed and loaded.";
+} else {
+    echo "PDO PostgreSQL driver is not installed or not loaded.";
+}
 ?>
